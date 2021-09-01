75d7e4b4c42f5e567c41c30af1503caae20cb95f
MoisesMorandini <moises-morandini@hotmail.com>
2021-08-31 21:13:11 -0300

    changed User/create page to become responsive

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import { Header } from "../../components/Header";
-import { SideBar } from "../../components/SideBar";
+import { Sidebar } from "../../components/Sidebar";
 import React from "react";
 import { Input } from "../../components/Form/Input";
 
@@ -9,17 +9,17 @@ export default function CreateUser() {
     <Box>
       <Header />
       <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6" >
-        <SideBar />
-        <Box flex="1" p="8" borderRadius={8} bg="gray.800">
+        <Sidebar />
+        <Box flex="1" p={["6", "8"]} borderRadius={8} bg="gray.800">
           <Heading size='large' fontWeight='normal'>Criar usuário</Heading>
           <Divider my='6' boderColor='gray.700' />
 
           <VStack spacing='8'>
-            <SimpleGrid minChildWidth='240px' spacing='8' width='100%'>
+            <SimpleGrid minChildWidth='240px' spacing={["6", "8"]} width='100%'>
               <Input name='name' label='Nome completo' />
               <Input name='email' label='E-mail' type='email' />
             </SimpleGrid>
-            <SimpleGrid minChildWidth='240px' spacing='8' width='100%'>
+            <SimpleGrid minChildWidth='240px' spacing={["6", "8"]} width='100%'>
               <Input name='password' type='password' label='Senha' />
               <Input name='password_confirmation' type='password' label='Confirmação da senha' />
             </SimpleGrid>
