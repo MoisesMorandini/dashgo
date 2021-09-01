8eab0a5c48601309714e498bbd7e86a337b6980a
MoisesMorandini <moises-morandini@hotmail.com>
2021-08-31 21:11:05 -0300

    changed User page \to become responsive

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Flex, Text, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Text, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import React from "react";
@@ -6,10 +6,15 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  });
+
   return (
     <Box>
       <Header />
-      <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6" >
+      <Flex w="100%" maxWidth={1480} my="6" mx="auto" px={["4", "4", "6"]} >
         <Sidebar />
         <Box flex="1" p="8" borderRadius={8} bg="gray.800">
           <Flex mb="8" justify="space-between" align="center">
@@ -23,17 +28,19 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+
+                {isWideVersion && <Th>Data de cadastro</Th>}
+
                 <Th width="8" />
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -42,16 +49,16 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">moises-morandini@hotmail.com</Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril, 2021</Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
                 <Td>
                   <Button as="a" size="sm" fontSize="sm" colorScheme="purple"
                     leftIcon={<Icon as={RiPencilLine} fontSize="18" />}>
-                    Editar
+                    {isWideVersion ? 'Editar' : ''}
                   </Button>
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -60,16 +67,16 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">moises-morandini@hotmail.com</Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril, 2021</Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
                 <Td>
                   <Button as="a" size="sm" fontSize="sm" colorScheme="purple"
                     leftIcon={<Icon as={RiPencilLine} fontSize="18" />}>
-                    Editar
+                    {isWideVersion ? 'Editar' : ''}
                   </Button>
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -78,11 +85,11 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">moises-morandini@hotmail.com</Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril, 2021</Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
                 <Td>
                   <Button as="a" size="sm" fontSize="sm" colorScheme="purple"
                     leftIcon={<Icon as={RiPencilLine} fontSize="18" />}>
-                    Editar
+                    {isWideVersion ? 'Editar' : ''}
                   </Button>
                 </Td>
               </Tr>
